57dfb7d6120a47aee4d62c26672583dacd19047b
harshilchandratre-capt <154047428+harshilchandratre-capt@users.noreply.github.com>
2023-12-26 20:58:47 +0530

    test-commit 01

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<!-- <div class="navbar bg-gradient-to-r from-slate-900 to-slate-700">
+    <div class="nav-left ">
+        <label for="brand">{{ $brand }}</label>
+    </div>
+    <div class="nav-right">
+        <label for="fyear">{{ $fYear }}</label>
+        <div class="close-button">{{ $closeButton }}</div>
+    </div>
+</div> -->
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> -->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Gauri Traders POS</title>
+
+    <!-- ✨ component css here 👇 -->
+    <!-- <link rel="stylesheet" href="/public/component-css/navbar.css"> -->
+
+
+
+
+
+    @vite('resources/css/app.css')
+
+</head>
+
+<body class="bg-gray-100 h-screen">
+
+    <!-- Navigation Bar -->
+    <!-- <x-navbar :brand="'Sale Return'" :fYear="'FY:2023'" :closeButton="'X'" /> -->
+    <!-- Navigation Bar -->
+
+
+    <!-- Main Content Area -->
+    <div class="main-content">
+        <h6>this is content</h6>
+    </div>
+
+    <!-- Add your scripts or other bottom-of-body content here -->
+
+</body>
+
+</html>
