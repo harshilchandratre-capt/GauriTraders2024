41954e3402ee634f8173363796f85d35b2941fd6
harshilchandratre-capt <harshilpcapt@gmail.com>
2024-01-14 10:04:39 +0530

    point-of-sale login integration -09harshil

--- resources/views/components/payment-details-pos.blade.php
+++ resources/views/components/payment-details-pos.blade.php
@@ -9,26 +9,128 @@
                 <div class="upi-method" onclick="openModal('upi')"><span>Ctrl + U - </span><b>UPI</b></div>
             </div>
             <div id="cashModal" class="modal">
-                <div class="form-container">
-                    <span class="close-btn" onclick="closeModal('cash')">&times;</span>
-                    <h2>Cash Payment Form</h2>
-                    <!-- Your cash payment form fields go here -->
+                <div class="popup">
+                    <div class="popup-title">
+                        <div class="header">
+                            <h2>Cash</h2>
+                            <span class="close-btn" onclick="closeModal('cash')"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 14.8594L7.92971 7.92972M7.92971 7.92972L14.8594 1M7.92971 7.92972L1 1M7.92971 7.92972L14.8594 14.8594" stroke="black" stroke-width="1.9827" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg></span>
+
+                        </div>
+                        <div class="separator"></div>
+
+
+                    </div>
+                    <div class="container">
+                        <div class="content">
+                            <div class="input-field">
+                                <label for="transaction-id">Cash Amount:</label>
+                                <input type="text" id="transaction-id" placeholder="529" />
+                            </div>
+                            <div class="buttons">
+                                <button class="save-btn">Save</button>
+                                <button class="cancel-btn">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
+
             <div id="cardModal" class="modal">
-                <div class="form-container">
-                    <span class="close-btn" onclick="closeModal('card')">&times;</span>
-                    <h2>Credit/Debit Card Payment Form</h2>
-                    <!-- Your card payment form fields go here -->
+                <div class="popup-card">
+                    <div class="popup-title">
+                        <div class="header">
+                            <h2>Credit/Debit Card Method</h2>
+                            <span class="close-btn" onclick="closeModal('card')"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 14.8594L7.92971 7.92972M7.92971 7.92972L14.8594 1M7.92971 7.92972L1 1M7.92971 7.92972L14.8594 14.8594" stroke="black" stroke-width="1.9827" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg></span>
+
+                        </div>
+                        <div class="separator"></div>
+
+
+                    </div>
+                    <div class="container">
+                        <div class="content">
+                            <div class="input-field">
+                                <label for="transaction-id">Transaction ID:</label>
+                                <input type="text" id="transaction-id" placeholder="57987298529" readonly />
+                            </div>
+                            <div class="input-field">
+                                <label for="upi-amount">card 1:</label>
+                                <input type="number" id="card 1" placeholder="6525 5354 5658 4547" readonly />
+                            </div>
+                            <div class="input-field">
+                                <label for="upi-amount">card 2:</label>
+                                <input type="number" id="card 1" placeholder="6525 5354 5658 4547" readonly />
+                            </div>
+                        </div>
+                        <div class="buttons">
+                            <button class="save-btn">Save</button>
+                            <button class="cancel-btn">Cancel</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div id="chequeModal" class="modal">
-                <div class="form-container">
-                    <span class="close-btn" onclick="closeModal('cheque')">&times;</span>
-                    <h2>Cheque Payment Form</h2>
-                    <!-- Your cheque payment form fields go here -->
+                <div class="popup-cheque">
+                    <div class="popup-title">
+                        <div class="header">
+                            <h2>Cheque Method</h2>
+                            <span class="close-btn" onclick="closeModal('cheque')"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 14.8594L7.92971 7.92972M7.92971 7.92972L14.8594 1M7.92971 7.92972L1 1M7.92971 7.92972L14.8594 14.8594" stroke="black" stroke-width="1.9827" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg></span>
+
+                        </div>
+                        <div class="separator"></div>
+
+                    </div>
+                    <div class="container">
+                        <div class="content">
+                            <div class="bank">
+                                <div class="input-field">
+                                    <label for="transaction-id">Bank Name:</label>
+                                    <input type="text" id="transaction-id" placeholder="State Bank of India" readonly />
+                                </div>
+                                <div class="input-field">
+                                    <label for="transaction-id">A/c Number:</label>
+                                    <input type="number" id="transaction-id" placeholder="52921641556416" readonly />
+                                </div>
+                                <div class="input-field">
+                                    <label for="transaction-id">IFSC Code:</label>
+                                    <input type="text" id="transaction-id" placeholder="SBIOOOO619" readonly />
+                                </div>
+                                <div class="input-field">
+                                    <label for="transaction-id">Beneficiary Name:</label>
+                                    <input type="text" id="transaction-id" placeholder="Abhishek Upadhyay" readonly />
+                                </div>
+
+                            </div>
+
+                            <div class="cheque">
+                                <div class="input-field">
+                                    <label for="transaction-id">Cheque No:</label>
+                                    <input type="number" id="transaction-id" placeholder="00619" readonly />
+                                </div>
+                                <div class="input-field">
+                                    <label for="transaction-id">Cheque Amount:</label>
+                                    <input type="number" id="transaction-id" placeholder="529" readonly />
+                                </div>
+                                <div class="input-field">
+                                    <label for="transaction-id">Cheque Date:</label>
+                                    <input type="date" id="transaction-id" placeholder readonly />
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="buttons">
+                            <button class="save-btn">Save</button>
+                            <button class="cancel-btn">Cancel</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -70,7 +172,7 @@
                         <input type="number" name="Customer Saving" id="net-amount" value="5725" readonly />
                     </div>
                     <div class="billing-amount-field-right">
-                        <span>received Amount </span>
+                        <span>Received Amount </span>
                         <input type="number" name="Received amount" id="net-amount" value="5725" readonly />
                     </div>
                     <div class="billing-amount-field-right">
@@ -86,7 +188,7 @@
     </div>
     <div class="payment-bottom">
         <div class="table">
-            <p> Table contant here</p>
+            <p> Table content here</p>
         </div>
     </div>
 
